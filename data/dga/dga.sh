dacca23c6aabd8cc7d9d40375dda88c005f50a11
Juhong-Namgung <wnghd9999@naver.com>
2020-05-05 16:56:24 +0900

    Add DGA and non-DGA dataset download script

---
+++ data/dga/dga.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get ONINT BAMBENEK CONSULTING
+wget -q --show-progress https://osint.bambenekconsulting.com/feeds/dga-feed.txt
+
+# Finished
+echo "[+] Saved to dga-feed.txt"
---
+++ data/dga/majestic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get ONINT BAMBENEK CONSULTING
+wget -q --show-progress http://downloads.majestic.com/majestic_million.csv
+
+# Finished
+echo "[+] Saved to majestic_million.csv"
