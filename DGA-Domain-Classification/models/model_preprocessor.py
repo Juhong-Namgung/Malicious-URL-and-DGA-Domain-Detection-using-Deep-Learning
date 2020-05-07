e02616ca799ab4c827a358d26c6a70647ec9c400
Juhong Namgung <wnghd9999@naver.com>
2020-05-07 11:40:48 +0900

    Update DGA model preprocessor

--- DGA-Domain-Classification/models/model_preprocessor.py
+++ DGA-Domain-Classification/models/model_preprocessor.py
@@ -1,38 +1,35 @@
 import pandas as pd
 import numpy as np
-import os
-import json
 from sklearn import model_selection
 
 from keras.preprocessing import sequence
 from keras.preprocessing.text import Tokenizer
 from keras.utils import np_utils
 from keras import backend as K
-from pathlib import Path
+
 
 class Preprocessor:
     def __init__(self):
         pass
 
-    # General save model to disk function
-    def save_model(model, fileModelJSON, fileWeights):
-        if Path(fileModelJSON).is_file():
-            os.remove(fileModelJSON)
-        json_string = model.to_json()
-        with open(fileModelJSON, 'w') as f:
-            json.dump(json_string, f)
-        if Path(fileWeights).is_file():
-            os.remove(fileWeights)
-        model.save_weights(fileWeights)
-
-    # Load and preprocess data
+    @staticmethod
     def load_data(__self__):
-        # Load data
-        DATA_HOME ='../data/'
-        df = pd.read_csv(DATA_HOME + 'dga_label.csv',encoding='ISO-8859-1', sep=',')
+        """ Load and pre-process data.
 
-        # Convert domain string to integer
-        # URL 알파벳을 숫자로 변경
+        1) Load data from dir
+        2) Tokenizing
+        3) Padding
+
+        return train and test data
+
+        """
+
+        # Load data
+        data_home = '../data/'
+        df = pd.read_csv(data_home + 'dga_label.csv', encoding='ISO-8859-1', sep=',')
+
+        # Tokenizing domain string on character level
+        # domain string to vector
         tokenizer = Tokenizer(filters='', lower=True, char_level=True)
         tokenizer.fit_on_texts(df.domain)
         url_int_tokens = tokenizer.texts_to_sequences(df.domain)
@@ -41,30 +38,42 @@ class Preprocessor:
         # 최대길이 67로 지정
         max_len = 67
 
-        X = sequence.pad_sequences(url_int_tokens, maxlen=max_len, padding='post')
+        x = sequence.pad_sequences(url_int_tokens, maxlen=max_len)
         y = np.array(df['class'])
 
         # Cross-validation
-        X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1, random_state=33)
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.1, random_state=33)
 
         # DGA class: 0~20: 21개
         y_train_category = np_utils.to_categorical(y_train, 21)
         y_test_category = np_utils.to_categorical(y_test, 21)
 
-        return X_train, X_test, y_train_category, y_test_category
+        return x_train, x_test, y_train_category, y_test_category
 
+    '''
+    Training Metrics
+    
+    1) precision
+    2) recall
+    3) F1-score
+        
+    '''
+
+    @staticmethod
     def precision(self, y_true, y_pred):
         true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
         predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
         precision = true_positives / (predicted_positives + K.epsilon())
         return precision
 
+    @staticmethod
     def recall(self, y_true, y_pred):
         true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
         possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
         recall = true_positives / (possible_positives + K.epsilon())
         return recall
 
+    @staticmethod
     def fbeta_score(self, y_true, y_pred, beta=1):
         if beta < 0:
             raise ValueError('The lowest choosable beta is zero (only precision).')
@@ -79,8 +88,6 @@ class Preprocessor:
         fbeta_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
         return fbeta_score
 
+    @staticmethod
     def fmeasure(self, y_true, y_pred):
         return Preprocessor.fbeta_score(self, y_true, y_pred, beta=1)
-
-processor = Preprocessor()
-processor.load_data()
