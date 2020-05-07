91ad759db4e4b6172d9f5eb327f4fea39fce3c44
Juhong Namgung <wnghd9999@naver.com>
2020-05-07 14:44:57 +0900

    Refactor DGA LSTM To use Keras Sequential

--- DGA-Domain-Classification/models/LSTM.py
+++ DGA-Domain-Classification/models/LSTM.py
@@ -1,79 +1,88 @@
 # Load Libraries
-import warnings
 
-import model_evaluator
-import model_preproecessor
+import warnings
+from datetime import datetime
+
 import tensorflow as tf
+from keras import backend as K
 from keras import regularizers
 from keras.callbacks import EarlyStopping
 from keras.callbacks import ModelCheckpoint
-from keras.layers import Input, LSTM, Embedding
+from keras.layers import Embedding, LSTM
 from keras.layers.core import Dense, Dropout
-from keras.models import Model
-from keras.models import load_model
+from keras.models import Sequential
 from keras.optimizers import Adam
+from model_evaluator import Evaluator
+from model_preprocessor import Preprocessor
 
 warnings.filterwarnings("ignore")
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.9
+K.tensorflow_backend.set_session(tf.Session(config=config))
 
 with tf.device("/GPU:0"):
 
-    def simple_lstm(max_len=67, emb_dim=32, max_vocab_len=38, lstm_output_size=32, W_reg=regularizers.l2(1e-4)):
-        # Input
-        main_input = Input(shape=(max_len,), dtype='int32', name='main_input')
-
-        # Embedding layer
-        emb = Embedding(input_dim=max_vocab_len, output_dim=emb_dim,
-                        input_length=max_len, dropout=0.2, W_regularizer=W_reg)(main_input)
-
-        # LSTM layer
-        lstm = LSTM(lstm_output_size)(emb)
-        lstm = Dropout(0.5)(lstm)
-
-        # Output layer (last fully connected layer)
-        output = Dense(21, activation='softmax', name='output')(lstm)
-
-        # Compile model and define optimizer
-        model = Model(input=[main_input], output=[output])
-        adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-        model.compile(optimizer=adam, loss='categorical_crossentropy',  metrics=['accuracy', tf.keras.metrics.CategoricalAccuracy(), preprocess.fmeasure, preprocess.recall, preprocess.precision])
+    def lstm(max_len=67, emb_dim=32, max_vocab_len=39, W_reg=regularizers.l2(1e-4)):
+        model = Sequential()
+        model.add(Embedding(input_dim=max_vocab_len, output_dim=emb_dim, input_length=max_len, W_regularizer=W_reg))
+        model.add(Dropout(0.5))
+        model.add(LSTM(128))
+        model.add(Dropout(0.5))
+        model.add(Dense(21, activation='softmax'))
 
         return model
 
 with tf.device("/GPU:0"):
-    epochs = 10
-    batch_size = 64
 
     # Load data using model preprocessor
-    preprocess = model_preproecessor.Preprocessor()
+    x_train, x_test, y_train, y_test = Preprocessor.load_data()
 
-    X_train, X_test, y_train, y_test = preprocess.load_data()
-
-    # define CNN model
-    model_name = "LSTM"
-    model = simple_lstm()
+    # Define Deep Learning Model
+    model_name = "1DCNN"
+    model = lstm()
 
     # Define early stopping
     es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
     mc = ModelCheckpoint('./trained_models/' + model_name+ '.h5', monitor='val_loss', mode='min', save_best_only=True)
 
-    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.11, callbacks=[es, mc])
+    ''' Training phrase '''
+    epochs = 10
+    batch_size = 64
+    adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 
-    saved_model = load_model('./trained_models/' + model_name+ '.h5', compile=False)
-    y_pred = saved_model.predict(X_test, batch_size=64)
+    model.compile(optimizer=adam, loss='categorical_crossentropy',
+                  metrics=['accuracy', tf.keras.metrics.CategoricalAccuracy(),
+                           Evaluator.precision, Evaluator.recall, Evaluator.fmeasure])
 
-    evaluator = model_evaluator.Evaluator()
+    dt_start_train = datetime.now()
+
+    history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.11, callbacks=[es, mc])
+
+    dt_end_train = datetime.now()
+
+    ''' Predict phrase '''
+    best_model = lstm()
+    best_model.load_weights('./trained_models/' + model_name+ '.h5')
+    best_model.compile(optimizer=adam, loss='categorical_crossentropy',
+                       metrics=['accuracy', tf.keras.metrics.CategoricalAccuracy(),
+                                Evaluator.precision, Evaluator.recall, Evaluator.fmeasure])
+
+    dt_start_predict = datetime.now()
+
+    y_pred = best_model.predict(x_test, batch_size=64)
+
+    dt_end_predict = datetime.now()
 
     # Validation curves
-    evaluator.plot_validation_curves(model_name, history)
-    evaluator.print_validation_report(history)
+    Evaluator.plot_validation_curves(model_name, history)
+    Evaluator.print_validation_report(history)
 
     # Experimental result
-    evaluator.calculate_measrue(saved_model, X_test, y_test)
-
-    # model.summary()
+    Evaluator.calculate_measure(best_model, x_test, y_test)
 
     # Save confusion matrix
-    evaluator.plot_confusion_matrix(model_name, y_test, y_pred, title='Confusion matrix', normalize=True)
+    Evaluator.plot_confusion_matrix(model_name, y_test, y_pred, title='Confusion matrix', normalize=True)
 
-    # Save final training model
-    # save_model("../models/" + model_name + ".json", "../models/" + model_name + ".h5")
+    # Print Training and predicting time
+    print('Train time: ' + str((dt_end_train - dt_start_train)))
+    print('Predict time: ' + str((dt_end_predict - dt_start_predict)))
