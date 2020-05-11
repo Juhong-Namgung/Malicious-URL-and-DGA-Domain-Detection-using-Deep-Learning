13ca1fffcb7e83e5a02b3cff27331d73fd5b211b
Juhong-Namgung <wnghd9999@naver.com>
2020-05-11 15:33:59 +0900

    Add benign URL data(script)

---
+++ Malicious-URL-Detection/data/benign/Malicious-Benign-Webpages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# [Cite this dataset] 
+#  Singh, AK (2020), “Dataset of Malicious and Benign Webpages”, Mendeley Data, v1
+#  http://dx.doi.org/10.17632/gdx3pkwp47.1
+
+wget https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/gdx3pkwp47-1.zip
